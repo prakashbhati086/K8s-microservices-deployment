83510868daf40b3d17d0212a8bb2b949ac27e4f2
prakashbhati086 <bhatiprakash086@gmail.com>
2025-08-20 21:33:41 +0530

    updated auth-service

--- auth-service/app/app.js
+++ auth-service/app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const session = require('express-session');
-const authRoutes = require('./routes/auth');
+const authRoutes = require('../routes/auth');
 
 dotenv.config();
 const app = express();
